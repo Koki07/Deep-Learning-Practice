d0cf0b478ce1f47fcf91dbe80997ecd15d4cb87d
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-12-22 14:05:18 +0900

    update activationFunction.py

---
+++ chapter4/activationFunction.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+plt.show()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+plt.show()
+
+
+def relu(x):
+    return np.maximum(0, x)  # 0 か x のどちらか大きい方を出力する
+
+
+y = relu(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+plt.show()
